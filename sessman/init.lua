5ffc147d74b1bf08946ebc9e35191a8a787355bf
IsoLinearCHiP <isolin.chip@gmail.com>
2013-02-17 22:32:54 +0100

    integrate the persistance functions. WIP

--- sessman/init.lua
+++ sessman/init.lua
@@ -36,6 +36,8 @@ local capi = {
     luakit = luakit
 }
 
+local json = require("sessman/json")
+
 -- Advanced sessionmanager inspired by SessionManager Extension to Firefox
 module("sessionman")
 
@@ -155,9 +157,144 @@ Session = {
         setmetatable(o, Session)
         -- print("creating new Session")
         return o
-    end
+    end,
+
+    dump = function(self)
+        return json.encode(self.win)
+    end,
+
+    sload = function(self, str)
+        self.win = json.decode(str)
+        return self.win
+    end,
 }
 
+
+----------------------------------------
+-- session loading and storing functions
+-- from sess_saver.lua (see attrib)
+----------------------------------------
+
+-- Utility functions.
+local function rm(file)
+    luakit.spawn(string.format("rm %q", file))
+end
+
+local function file(path,fname)
+    return path .. "/" .. fname
+end
+
+-- Session functions.
+session = {
+    -- The directory where sessions are stored
+    path = "/directory/store/luakit-sessions",
+
+    -- Save all tabs of current window to session file (if it exists).
+    save = function (w, session_data, force)
+        -- abort if no session_data or empty name
+        if not session_data or not session_data.name then return false end
+
+        local name = session_data.name
+        if name then
+            -- create a structure, { {if current, history}, ... }
+            local current = w.tabs:current()
+            local tabs = {}
+            for ti, tab in ipairs(w.tabs.children) do
+                table.insert(tabs, { current == ti, tab.history })
+            end
+            -- do the saving
+            if #tabs > 0 then
+                if session_name then
+                    if session.write(name,tabs,force) then
+                        w:notify("\"" .. name .. "\" written")
+                        if not w.session then session.setname(w,name,true) end
+                    else
+                        w:error("\"" .. name .. "\" exists in session directory (add ! to override)")
+                    end
+                else
+                    if "old" == session.write(name,tabs,true) then
+                        w:notify("\"" .. name .. "\" written")
+                    else
+                        w:notify("\"" .. name .. "\" [New] written")
+                    end
+                end
+            else -- remove session
+                os.remove(file(session.path,name)) -- delete file from system
+                w:warning("\"" .. name .. "\" removed")
+            end
+        else
+            w:error("No session name")
+        end
+    end,
+
+    -- Write tab data.
+    write = function (name, session, force)
+        local sfile = file(session.path,name) -- will save to path/name
+        local age = os.exists(sfile) and "old" or "new"
+        if age == "old" and not force then return false end
+        local fh = io.open(sfile, "w")
+        fh:write(session:dump())
+        io.close(fh)
+        return age
+    end,
+
+    -- Set the name of the session for the window.
+    setname = function (w, name, force)
+        if os.exists(file(session.path,name)) and not force then return false end
+        w.session = name
+        w.view:emit_signal("property::session-name")
+        return true
+    end,
+
+    -- Load new session from file; optionally replace existing session.
+    sload = function (w, name, replace)
+        if name then
+            local tabs = session.read(name)
+            if tabs then
+                if replace then
+                    -- clear tabs from current window
+                    while w.tabs:count() ~= 0 do
+                        w:close_tab(nil, false)
+                    end
+                end
+                session.open(w,tabs)
+                if replace then
+                    session.setname(w,name,true)
+                end
+                if replace then
+                    w:notify("\"" .. name .. "\" loaded")
+                else
+                    w:notify("\"" .. name .. "\" merged")
+                end
+            else
+                w:error("\"" .. name .. "\" does not exist")
+            end
+        else
+            w:error("No session name")
+        end
+    end,
+
+    -- Read urls from session file.
+    read = function (name)
+        local path = session.path
+        local sfile = file(path,name)
+        if not os.exists(sfile) then return end
+        local fh = io.open(sfile, "r")
+        local sess = Session:new()
+        sess:sload(fh:read("*all"))
+        io.close(fh)
+        return sess
+    end,
+
+    -- Open new tabs from table of tab data.
+    open = function (w,tabs)
+        if tabs and w then -- load new tabs
+            for _, tab in ipairs(tabs) do
+                w:new_tab(tab[2], tab[1])
+            end
+        end
+    end,
+}
 
 -------------------------
 -- JS interface functions
